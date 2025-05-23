1543a506d7a66e79a3db7691729345aa504fa44c
caoyuan1231 <159030509+caoyuan1231@users.noreply.github.com>
2025-05-23 19:52:06 +0800

    Add files via upload

---
+++ Liftoff.sh
@@ -0,0 +1,3 @@
+liftoff -g ./IWGSC_v1.1_HC_20170706.gff3 -o fld.genome.gff3 -p 32 -chroms chr.map -u ./unmap.txt ./201216_Fielder_pseudomolecules_V1+unanchored_contigs.fasta ./Chinese_Spring_v1.0_pseudomolecules.fasta
+
+liftoff -g ./IWGSC_v1.1_HC_20170706.gff3 -o jm22.genome.gff3 -p 16 -chroms chr.map -u ./unmap.txt ../GWHEQUK00000000.genome.fasta./Chinese_Spring_v1.0_pseudomolecules.fasta
